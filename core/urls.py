09168987b4d194260693800f6977ba960604b902
semre <sekarabela@gmail.com>
2017-12-06 23:51:36 +0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(success_url='/'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('brew/', views.BrewView.as_view(), name='brew'),
     path('brew_update_rate/<int:pk>/<int:rate>/', views.BrewUpdateRate.as_view(), name='brew-update-rate'),
     path('recipe/', views.RecipeView.as_view(), name='recipe'),
