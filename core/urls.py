c998b287a6c27f1c7f3a9a05575326f7dc8de617
KadirEmreOto <emrekadiroto@hotmail.com>
2017-12-06 20:37:16 +0300

    implemented brew view class and added debug toolbar

--- core/urls.py
+++ core/urls.py
@@ -6,7 +6,9 @@ app_name = 'core'
 
 urlpatterns = [
     url(r'^recipe/(?P<recipe_id>[0-9]+)/$', views.RecipeView.as_view(), name='recipe'),
+    url(r'^brew/$', views.BrewView.as_view(), name='brew'),
     url(r'^profile/$', views.IngredientView.as_view(), name='profile'),
     url(r'^profile/$', views.IngredientView.as_view(), name='login'),
+    url(r'^logout/$', views.IngredientView.as_view(), name='logout'),
     url(r'^profile/$', views.IngredientView.as_view(), name='register'),
 ]
