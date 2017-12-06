88a789f4121732f820803e7c8a4d374c431868d6
KadirEmreOto <emrekadiroto@hotmail.com>
2017-12-06 23:15:08 +0300

    added rates and comments to brews

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.views import View
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Recipe, Ingredient, Brew
 
 
@@ -31,6 +31,43 @@ class IngredientView(View):
 class BrewView(View):
     @staticmethod
     def get(request):
-        brews = Brew.objects.filter(recipe__user=request.user).order_by('date')[:10].\
+        brews = Brew.objects.filter(recipe__user=request.user).order_by('-rate', 'date')[:10].\
             select_related('recipe').prefetch_related('recipe__ingredient_set')
+
         return render(request, 'brew.html', locals())
+
+    @staticmethod
+    def post(request):
+        for name in request.POST:
+            if name.startswith('comment'):
+                print(name)
+                pk = int(name.split('-')[-1])
+                brew = Brew.objects.get(pk=pk)
+
+                if brew.recipe.user != request.user:
+                    return HttpResponse('Unauthorized', status=401)
+
+                brew.note = request.POST[name]
+                brew.save()
+                break
+
+        return redirect('core:brew')
+
+
+class BrewUpdateRate(View):
+    @staticmethod
+    def get(request, pk, rate):
+        brew = Brew.objects.get(pk=pk)
+        rate = int(rate)
+
+        if brew.recipe.user != request.user:
+            return HttpResponse('Unauthorized', status=401)
+
+        if rate not in range(1, 6):
+            print(rate, '*'*10)
+            return HttpResponse('Bad Request', status=400)
+
+        brew.rate = rate
+        brew.save()
+
+        return redirect('core:brew')
