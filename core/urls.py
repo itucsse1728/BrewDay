e112bc340ad9e227a4b18a726c20c2aadcdcf9be
yasakci <yasakci@itu.edu.tr>
2017-12-06 20:51:05 +0300

    added recipe view and recipe page

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'core'
 
 urlpatterns = [
-    url(r'^recipe/(?P<recipe_id>[0-9]+)/$', views.RecipeView.as_view(), name='recipe'),
+    url(r'^recipe/$', views.RecipeView.as_view(), name='recipe'),
     url(r'^profile/$', views.IngredientView.as_view(), name='profile'),
     url(r'^profile/$', views.IngredientView.as_view(), name='login'),
     url(r'^profile/$', views.IngredientView.as_view(), name='register'),
