e558473f4e2c3913727129d73583b02d619b6706
KadirEmreOto <emrekadiroto@hotmail.com>
2017-12-11 21:20:38 +0300

    added brew and recipe tests

--- test/test.py
+++ test/test.py
@@ -1,3 +1,4 @@
+import time
 import random
 import string
 
@@ -6,15 +7,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Test(object):
-
     def __init__(self):
         self.site = 'http://127.0.0.1:8000'
         self.delay = 2  # seconds
-        self.username = 'ismail'
-        self.email = 'ismail@ismail.com'
-        self.password = 'namdar123'
+
         self.browser = None
 
+        self.username = 'keo'
+        self.email = ''
+        self.password = '123456gs'
+        self.ingredient_name = ""
+        self.ingredient_amount = '54.2'
+        self.recipe_name = f'NEW RECIPE #{random.randint(1000, 9999)}'
 
     def check_login(self):
         url = '/admin'
@@ -33,9 +37,8 @@ class Test(object):
         WebDriverWait(self.browser, self.delay).until(
             lambda x: x.find_element_by_xpath('//div[@id="user-tools"]'))
 
-    def check_brew(self):
+    def check_rate_brew(self):
         url = '/brew'
-
         self.browser.get(self.site + url)
 
         products = WebDriverWait(self.browser, self.delay).until(
@@ -63,6 +66,10 @@ class Test(object):
 
         assert random_rate == current_rate
 
+    def acheck_comment_brew(self):
+        url = '/brew'
+        self.browser.get(self.site + url)
+
         products = WebDriverWait(self.browser, self.delay).until(
             lambda x: x.find_elements_by_xpath('//div[@class="product"]'))
 
@@ -72,7 +79,7 @@ class Test(object):
         button = product.find_element_by_tag_name('button')
 
         textname = textarea.get_attribute('name')
-        random_comment = ''.join(random.sample(string.printable, 10))
+        random_comment = ''.join(random.sample(string.ascii_lowercase*10, 10))
 
         textarea.clear()
         textarea.send_keys(random_comment)
@@ -84,10 +91,8 @@ class Test(object):
 
         assert textarea.text == random_comment
 
-
     def check_profile(self):
         url = '/profile'
-
         self.browser.get(self.site + url)
 
         products = WebDriverWait(self.browser, self.delay).until(
@@ -102,27 +107,143 @@ class Test(object):
 
         product = products[1]
 
-        ingredients = product.find_element_by_tag_name('td')
-        name = ingredients[0].text
+        ingredient = product.find_element_by_tag_name('td')
+        self.ingredient_name = ingredient.text
 
-        product.find_element_by_tag_name('button')[0].click()
+        product.find_element_by_tag_name('button').click()
+        time.sleep(0.3)
 
         products = WebDriverWait(self.browser, self.delay).until(
             lambda x: x.find_elements_by_xpath('//div[@class="product"]'))
 
         product = products[1]
 
-        ingredients = product.find_element_by_tag_name('td')
+        ingredients = product.find_elements_by_tag_name('td')
 
         for ingredient in ingredients:
-            if ingredient.text == name:
+            if ingredient.text == self.ingredient_name:
                 assert False
 
-        assert True
+        new_name = product.find_element_by_xpath('//input[@name="new-ingredient-name"]')
+        new_amount = product.find_element_by_xpath('//input[@name="new-ingredient-amount"]')
 
+        new_name.send_keys(self.ingredient_name)
+        new_amount.send_keys(self.ingredient_amount)
+
+        button = product.find_element_by_xpath('//button[@name="submit"]')
+        button.click()
+        time.sleep(0.3)
+
+        product = WebDriverWait(self.browser, self.delay).until(
+            lambda x: x.find_elements_by_xpath('//div[@class="product"]'))[1]
+
+        rows = product.find_elements_by_tag_name('tr')
+        flag = False
+
+        for row in rows[:-1]:
+            cols = row.find_elements_by_tag_name('td')
+
+            try:
+                current_amount = cols[1].find_element_by_tag_name('input').get_attribute('placeholder')
+
+                if len(cols) == 3 and cols[0].text == self.ingredient_name and current_amount == self.ingredient_amount:
+                    flag = True
+                    break
+            except:
+                pass
+
+        assert flag
+
+    def check_add_recipe(self):
+        url = '/recipe'
+
+        self.browser.get(self.site + url)
+
+        product = WebDriverWait(self.browser, self.delay).until(
+            lambda x: x.find_element_by_xpath('//div[@class="product"]'))
+
+        recipe_name_input = product.find_element_by_xpath('//input[@name="recipeName"]')
+        new_name_input = product.find_element_by_xpath('//input[@name="new-ingredient-name"]')
+        new_amount_input = product.find_element_by_xpath('//input[@name="new-ingredient-amount"]')
+        create_button = product.find_element_by_xpath('//button[@name="submit"]')
+
+        recipe_name_input.send_keys(self.recipe_name)
+        new_name_input.send_keys(self.ingredient_name)
+        new_amount_input.send_keys(str(float(self.ingredient_amount)-40))
+
+        create_button.click()
+        time.sleep(0.3)
+
+        product = WebDriverWait(self.browser, self.delay).until(
+            lambda x: x.find_elements_by_xpath('//div[@class="product"]'))[-1]
+
+        assert product.find_element_by_xpath('.//ul[@class="product-labels"]').text.strip() == self.recipe_name
+
+    def check_create_brew(self):
+        url = '/recipe'
+        self.browser.get(self.site + url)
+
+        products = WebDriverWait(self.browser, self.delay).until(
+            lambda x: x.find_elements_by_xpath('//div[@class="product"]'))
+
+        product = None
+
+        for p in products:
+            p_name = p.find_element_by_xpath('.//ul[@class="product-labels"]').text.strip()
+            if p_name == self.recipe_name:
+                product = p
+
+        assert product
+
+        brew_button = product.find_element_by_xpath('.//button[@name="brew"]')
+        brew_button.click()
+        time.sleep(0.3)
+
+        url = '/brew'
+        self.browser.get(self.site + url)
+
+        products = WebDriverWait(self.browser, self.delay).until(
+            lambda x: x.find_elements_by_xpath('//div[@class="product"]'))
+
+        flag = False
+        for p in products:
+            p_name = p.find_element_by_xpath('.//ul[@class="product-labels"]').text.strip()
+            if p_name == self.recipe_name:
+                flag = True
+                break
+
+        assert flag
+
+    def check_delete_recipe(self):
+        url = '/recipe'
+        self.browser.get(self.site + url)
+
+        products = WebDriverWait(self.browser, self.delay).until(
+            lambda x: x.find_elements_by_xpath('//div[@class="product"]'))
+
+        product = None
+
+        for p in products:
+            p_name = p.find_element_by_xpath('.//ul[@class="product-labels"]').text.strip()
+            if p_name == self.recipe_name:
+                product = p
+
+        assert product
+
+        delete_button = product.find_element_by_xpath('.//button[@name="delete"]')
+        delete_button.click()
+        time.sleep(0.3)
+
+        products = WebDriverWait(self.browser, self.delay).until(
+            lambda x: x.find_elements_by_xpath('//div[@class="product"]'))
+
+        for p in products:
+            p_name = p.find_element_by_xpath('.//ul[@class="product-labels"]').text.strip()
+            if p_name == self.recipe_name:
+                assert False
 
     def run(self):
-        self.browser = webdriver.Chrome('/Users/ismail/Downloads/chromedriver')
+        self.browser = webdriver.Firefox()
 
         for attr in __class__.__dict__:
             if callable(getattr(self, attr)) and attr.startswith('check'):
